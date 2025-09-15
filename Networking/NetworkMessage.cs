01576d26f8598e0f28d7543800d420c7c2585d39
Elliotto201 <elliotto.furmanski@gmail.com>
2025-09-15 23:55:48 +0200

    Network Message
    Added a NetworkMessage struct that has support for diffrent types of messages. Aswell as a serializer for it that can efficientley serialize AND compress it.

---
+++ Networking/NetworkMessage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networking
+{
+    internal struct NetworkMessage
+    {
+        public ushort PacketId;
+        public PacketFlag PacketFlags;
+        public PacketType PacketType;
+
+        public byte[] Payload;
+    }
+
+    [Flags]
+    internal enum PacketFlag : byte
+    {
+        Reliable = 1,
+        Ordered = 2,
+        Compressed = 4,
+    }
+
+    internal enum PacketType : byte
+    {
+        Data,
+        Ack,
+        ClientConnectRequest,
+        ClientDisconnectRequest,
+        ClientKick,
+    }
+}
---
+++ Networking/NetworkMessageSerializer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace Networking
+{
+    internal static class NetworkMessageSerializer
+    {
+        public static byte[] SerializeMessage(NetworkMessage packet)
+        {
+            using var ms = new MemoryStream();
+            using var bw = new BinaryWriter(ms);
+
+            bw.Write(packet.PacketId);
+            bw.Write((byte)packet.PacketFlags);
+            bw.Write((byte)packet.PacketType);
+
+            if (!packet.PacketFlags.HasFlag(PacketFlag.Compressed))
+            {
+                bw.Write(packet.Payload);
+            }
+            else
+            {
+                using var compressed = new MemoryStream();
+                using (var deflate = new DeflateStream(compressed, CompressionLevel.Fastest, true))
+                {
+                    deflate.Write(packet.Payload, 0, packet.Payload.Length);
+                }
+                bw.Write(compressed.ToArray());
+            }
+
+            return ms.ToArray();
+        }
+
+        public static NetworkMessage DeserializeMessage(byte[] bytes)
+        {
+            var packet = new NetworkMessage();
+
+            using var ms = new MemoryStream(bytes);
+            using var br = new BinaryReader(ms);
+
+            packet.PacketId = br.ReadUInt16();
+            packet.PacketFlags = (PacketFlag)br.ReadByte();
+            packet.PacketType = (PacketType)br.ReadByte();
+
+            int payloadLength = (int)(ms.Length - ms.Position);
+            byte[] payloadData = br.ReadBytes(payloadLength);
+
+            if (!packet.PacketFlags.HasFlag(PacketFlag.Compressed))
+            {
+                packet.Payload = payloadData;
+            }
+            else
+            {
+                using var input = new MemoryStream(payloadData);
+                using var deflate = new DeflateStream(input, CompressionMode.Decompress);
+                using var output = new MemoryStream();
+                deflate.CopyTo(output);
+                packet.Payload = output.ToArray();
+            }
+
+            return packet;
+        }
+    }
+}
