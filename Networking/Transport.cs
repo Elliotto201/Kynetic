5b639d0adbbbe346c251438dd49b59191a4954c9
Elliotto201 <elliotto.furmanski@gmail.com>
2025-09-16 12:10:39 +0200

    Network Transport
    Network Transport class created. Can send and receive messages.

---
+++ Networking/Transport.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networking
+{
+    internal sealed class Transport
+    {
+#pragma warning disable CS8618
+        private UdpClient client;
+#pragma warning restore CS8618
+
+        public void Start(int port)
+        {
+            client = new UdpClient(port);
+        }
+
+        public void Stop()
+        {
+            client.Close();
+            client.Dispose();
+        }
+
+        public async Task SendAsync(IPEndPoint endPoint, byte[] data)
+        {
+            CheckInit();
+            
+            await client.SendAsync(data, endPoint);
+        }
+
+        public async Task<(IPEndPoint endPoint, byte[] data)> ReceiveAsync()
+        {
+            CheckInit();
+
+            var receiveData = await client.ReceiveAsync();
+
+            return (receiveData.RemoteEndPoint, receiveData.Buffer);
+        }
+
+        private void CheckInit()
+        {
+            if(client == null)
+            {
+                throw new InvalidOperationException("The transport was not initialized");
+            }
+        }
+    }
+}
